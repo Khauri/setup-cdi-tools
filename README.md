6fe7edc5c6b904a105f48fb2ffe387e3df23fcf6
khauri <kmcclain@email.wm.edu>
2024-10-10 13:15:10 -0400

    feat: greatly simplify tool setup, allow custom tools, switch to bun in dev, improve documentation

--- README.md
+++ README.md
@@ -1,28 +1,9 @@
 A GitHub Action to set up a variety of tools for Continuous Delivery.
 
-## The folk's feature
+## Features
 
-- bump dependencies in the `package.json`
-- bump versions of components to download in integration test
-- update deprecated fields in the workflows and action config
-- allows arm64 packages to be installed
-
-## Supported Tools
-
-The tool matching the architecture of the action runner (either amd64 or arm64) will automatically be downloaded.
-
-Consult the following support table to see if a build for the target architecture is provided. Be sure to select a version of the tool that has a build for the desired architecture. 
-
-|  | linux | macos | windows |
-|---|:---:|---|---|
-| [jq](https://stedolan.github.io/jq/) | amd64,arm64 | amd64,arm64 | amd64 |
-| [yq](https://github.com/mikefarah/yq) | amd64,arm64 | amd64,arm64 | amd64 |
-| [kubectl](https://github.com/kubernetes/kubectl) | amd64,arm64 | amd64,arm64 | amd64 |
-| [kustomize](https://github.com/kubernetes-sigs/kustomize) | amd64,arm64 | amd64,arm64 | amd64,arm64 |
-| [skaffold](https://github.com/GoogleContainerTools/skaffold) | amd64,arm64 | amd64,arm64 | amd64 |
-| [helm](https://github.com/helm/helm) | amd64,arm64 | amd64,arm64 | amd64 |
-| [hub](https://github.com/github/hub) | amd64,arm64 | amd64,arm64 | amd64 |
-| [oras](https://github.com/oras-project/oras) | amd64,arm64 | amd64,arm64 | amd64 |
+- Download and setup a variety of common CDI tools for multiple architectures and platforms (x64, arm64)
+- setup and download arbitrary custom tools
 
 ## Usage
 
@@ -51,12 +32,61 @@ jobs:
       ...
 ```
 
+## Built-in Tools
+
+The tool matching the architecture of the action runner (either amd64 or arm64) will automatically be downloaded.
+
+Consult the following support table to see if a build for the target architecture is provided. 
+When deciding the version of the tool that you would like to use, be sure to use one that has a build for the desired architecture.
+
+|  | linux | macos | windows |
+|---|:---:|---|---|
+| [jq](https://stedolan.github.io/jq/) | amd64,arm64 | amd64,arm64 | amd64 |
+| [yq](https://github.com/mikefarah/yq) | amd64,arm64 | amd64,arm64 | amd64 |
+| [kubectl](https://github.com/kubernetes/kubectl) | amd64,arm64 | amd64,arm64 | amd64 |
+| [kustomize](https://github.com/kubernetes-sigs/kustomize) | amd64,arm64 | amd64,arm64 | amd64,arm64 |
+| [skaffold](https://github.com/GoogleContainerTools/skaffold) | amd64,arm64 | amd64,arm64 | amd64 |
+| [helm](https://github.com/helm/helm) | amd64,arm64 | amd64,arm64 | amd64 |
+| [hub](https://github.com/github/hub) | amd64,arm64 | amd64,arm64 | amd64 |
+| [oras](https://github.com/oras-project/oras) | amd64,arm64 | amd64,arm64 | amd64 |
+
+## Custom Tools
+
+This feature was added in the event that you want to easily setup tools that are not built-in.
+
+> [!NOTE]
+> inputs to github actions can only be passed in as strings. Make sure that you are passing in this value as a 
+> [multi-line string](https://yaml-multiline.info/). The string itself should be yaml formatted
+
+```yaml
+custom: |
+  # Provide a direct URL to the tool's download link
+  # This method is the simplest to use when you already know the runner's platform and architecture  
+  my-tool: https://github.com/org/repo/releases/download/1.0.0/my-tool-linux-x64.tar.gz
+
+  # Provide a template + configuration
+  # This can be used to automatically determine which tool to download based on the type of runner
+  my-tool:
+    version: 1.0.0
+    template: https://github.com/org/repo/releases/download/${version}/my-tool-${platform}-${arch}.tar.gz
+    linux:
+      architectures: [x86, arm64]
+    windows:
+      architectures: [x86]
+      # use a different version on windows
+      version: 0.9.0
+    darwin:
+      architectures: [x86]
+      # Download from a different url
+      template: https://github.com/org/repo-mac/releases/download/${version}/my-tool 
+```
+
 ## Contributing
 
-Requires node >= 16 and yarn ^1 
+Requires Bun
 
 ```sh
-yarn install
+bun install
 ```
 
 Make changes and then run the build
@@ -64,7 +94,7 @@ Make changes and then run the build
 This repository does not currently have an automated build workflow setup and so builds are currently pushed and released manually
 
 ```sh
-yarn build
+bun run build
 ```
 
 Finally publish the changes
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,154 @@
 import * as core from '@actions/core';
-import { JqInstaller } from './installers/jq';
-import { YqInstaller } from './installers/yq';
-import { KubectlInstaller } from './installers/kubectl';
-import { KustomizeInstaller } from './installers/kustomize';
-import { SkaffoldInstaller } from './installers/skaffold';
-import { HelmInstaller } from './installers/helm';
-import { HubInstaller } from './installers/hub';
-import { Installer } from './installers/installer';
-import { OrasInstaller } from './installers/oras';
+import yaml from 'js-yaml';
+import * as tools from './tools.yml';
+import {getBinary, getTarballBinary} from './utils';
 
-async function run() {
-  for (const [tool, installer] of Object.entries(installers)) {
-    const version = core.getInput(tool);
-    if (version) {
-      core.info(`Run installer. tool: ${tool}, version: ${version}`);
-      await installer.install(version);
-    }
-  }
+type PlatformConfig = {
+  version?: string,
+  /**
+   * Usually the file extension of the downloaded tool will be used to determine if the tool is a binary or an archive.
+   * Use this option if the file extension is not enough to determine the type of the downloaded tool.
+   * When set to true the installer will extract the download as an archive using a default strategy.
+   * When set to 'zip' or 'tar' the installer will extract the downloaded archive using the specified method.
+   * When set to false the installer will assume a binary was downloaded and will not attempt unarchiving.
+   */
+  unArchive?: boolean|'zip'|'tar',
+  /**
+   * Indicates the relative path to the binary if the tool is within an archive.
+   * If not provided the installer will look for a file with the same name as the tool at the root of the repository.
+   * Provide this option if the binary is located in a subdirectory, has a different name, or there are multiple binaries in the archive.
+   */
+  binaryPath?: string,
+  /**
+   * A list of architectures supported by the tool
+   */
+  architectures?: {
+    x64?: boolean|PlatformConfig&{as?: string}, 
+    arm64?: boolean|PlatformConfig&{as?: string},
+  },
 
-  core.info('Finished installing tools successfully.');
+  /**
+   * The template used to generate the download URL
+   */
+  template?: string,
+
+  /**
+   * The file extension of the downloaded tool. Usually .exe for Windows binaries and empty for linux, but this can be overriden here
+   */
+  extension?: string,
 }
 
-const installers = {
-  jq: new JqInstaller(),
-  yq: new YqInstaller(),
-  kubectl: new KubectlInstaller(),
-  kustomize: new KustomizeInstaller(),
-  skaffold: new SkaffoldInstaller(),
-  helm: new HelmInstaller(),
-  hub: new HubInstaller(),
-  oras: new OrasInstaller(),
-} as { [tool: string]: Installer };
+type ToolConfig = PlatformConfig & {
+  platforms?: {
+    // Operating system specific settings
+    linux?: PlatformConfig & {as?: string},
+    darwin?: PlatformConfig & {as?: string},
+    windows?: PlatformConfig & {as?: string},
+  }
+};
+
+const platformMap = {
+  linux: 'linux',
+  darwin: 'darwin',
+  win32: 'windows',
+  windows: 'windows',
+} as const;
+
+const archMap = {
+  x64: 'x64',
+  amd64: 'x64',
+  arm64: 'arm64',
+} as const;
+
+function resolveTemplate(template: string, variables: Record<string, string>): string {
+  return template.replace(/\${([^}]+)}/g, (_, key) => {
+    return variables[key] || '';
+  });
+}
+
+function resolveConfig(config: String|ToolConfig): [PlatformConfig, Record<string, string>] {
+  const variables = {
+    platform: core.platform.platform as string,
+    arch: core.platform.arch,
+  };
+  if(typeof config === 'string') {
+    config = {template: config};
+  }
+
+  // Override the config with platform+arch specific settings
+  const settings: ToolConfig = {...config as ToolConfig};
+  
+  const platform = platformMap[core.platform.platform as keyof typeof platformMap];
+  // check if config has platform specific settings
+  const platformConfig = settings.platforms?.[platform];
+  if(platformConfig) {
+    Object.assign(settings, settings.platforms?.[platform]);
+    if(platformConfig.as) {
+      variables.platform = platformConfig.as;
+    }
+  }
+  delete settings.platforms;
+  // Check if architecture is supported
+  const arch = archMap[core.platform.arch as keyof typeof archMap];
+  if(typeof settings.architectures === 'undefined') {
+    // If no architectures are defined, assume all architectures are under the same configuration
+    return [settings, variables];
+  }
+  const archConfig = settings.architectures[arch];
+  if(!archConfig) {
+    // Unless explicitly defined, architecture is not supported for this tool
+    throw new Error(`Unsupported platform and architecture: ${platform}_${arch}`);
+  }
+  Object.assign(settings, archConfig);
+  if(typeof archConfig === 'object' && archConfig.as) {
+    variables.arch = archConfig.as;
+  }
+  delete settings.architectures;
+  return [settings, variables];
+}
+
+function install(toolName: string, config: PlatformConfig, variables: Record<string, string>) {
+  return core.group(`Install ${toolName}`, async () => {
+    const url = resolveTemplate(config.template!, {
+      ...variables,
+      version: config.version!,
+      extension: config.extension!,
+    });
+    const unArchive = config.unArchive ?? url.match(/(\.zip$|\.tar\.gz$|\.tgz$)/)?.[1] ?? false;
+    let {binaryPath} = config;
+    if(binaryPath) {
+      binaryPath = resolveTemplate(binaryPath, variables);
+    }
+    console.log({binaryPath, url});
+    // const toolPath = unArchive 
+    //   ? await getTarballBinary(toolName, config.version!, url)
+    //   : await getBinary(toolName, config.version!, url);
+    // TODO: switch from binaryPath to toolPath in this log
+    core.debug(`${toolName} has been cached at ${binaryPath}`);
+
+    // core.addPath(path.dirname(toolPath));
+  });
+}
+
+async function run() {
+  // Install built-in tools
+  for(const [tool, config] of Object.entries(tools as Record<string, String|ToolConfig>)) {
+    const version = core.getInput(tool);
+    if(version) {
+      const toolConfig = typeof config === 'string' 
+        ? {version, template: config} 
+        : {...config, version};
+      await install(tool, ...resolveConfig(toolConfig));
+    }
+  }
+  // Install custom tools
+  const customTools = core.getInput('custom');
+  if(customTools) {
+    const parsed = yaml.load(customTools) as Record<string, String|ToolConfig>;
+    for(const [tool, config] of Object.entries(parsed)) {
+      await install(tool, ...resolveConfig(config));
+    }
+  }
+}
 
 run().catch(core.setFailed);
